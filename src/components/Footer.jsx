98c8d3444c4f19d51db8ea6b214236f0ae868f3a
TangiLC <tangi@le-cadre.net>
2023-07-12 17:01:05 +0200

    dev start

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import logo from '../assets/logo.png';
+
+const Footer =() => {
+    return('FOOTER')
+}
+
+export default Footer;
---
+++ src/components/HeaderMenu.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import logo from '../assets/logo.png';
+
+const HeaderMenu =() => {
+    return('HEADER')
+}
+
+export default HeaderMenu;
