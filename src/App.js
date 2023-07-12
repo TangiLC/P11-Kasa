4867f7a8531e971c75d50047ab7e80b2f1fcebda
TangiLC <tangi@le-cadre.net>
2023-07-12 16:53:59 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from '../src/assets/logo.png';
+import React, { useState } from 'react';
+import { useParams, Routes, Route } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import Logement from './pages/Logement';
+import About from './pages/About';
+import Page404 from './pages/Page404';
+
+function SearchQuery() {
+  return useParams();
+}
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+            
+            <Route path="*" element={<Page404 />} />
+        </Routes>
   );
 }
 
 export default App;
+export { SearchQuery};
+
+
+/*
+<Route path="/" element={<HomePage />} />
+            <Route path="/HomePage" element={<HomePage />} />
+            <Route path="/Logement" element={<HomePage />} />
+            <Route path="/Logement/:id" element={<Logement />} />
+            <Route path="/About" element={<About />} />*/
---
+++ src/pages/Page404.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../styles/page404.css';
+
+const Page404 = () => {
+    return (
+        <>
+            <div className="error-container">
+                <span>
+                    <span>4</span>
+                </span>
+                <span>0</span>
+                <span>
+                    <span>4</span>
+                </span>
+            </div>
+            <div className="link-container">
+                Oups! La page que vous demandez n'existe pas.
+            </div>
+        </>
+    );
+};
+
+export default Page404;
