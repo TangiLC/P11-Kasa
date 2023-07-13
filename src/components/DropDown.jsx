517ecfb7284566a39f6edb988da9b959a50699ae
TangiLC <tangi@le-cadre.net>
2023-07-13 18:40:34 +0200

    dropdown

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -1,16 +1,27 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import arrow from '../assets/arrow_down.svg'
 
 import '../styles/dropdown.css';
 
 const DropDown = (props) => {
 
+  const [isOpen, setIsOpen] = useState(false)
+
+  function handleDropDown(e) {
+    e.preventDefault();
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <div className="drop-container"> /*key*/
-    <div className="drop-label">
+    <div className="drop-container"> 
+    <div className="drop-label" onClick={handleDropDown}>
       {props.label}
+      <div className={isOpen?"arrow-open":"arrow-closed"}>
+      <img src={arrow}  alt="cliquer pour modifier la vue"></img>
+      </div>
     </div>
-    <div className="drop-content">
+    <div className={isOpen?"drop-open":"drop-closed"}>
       {props.content}
     </div>
         
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,53 +5,25 @@ import '../styles/about.css';
 import TopPicture from '../components/TopPicture';
 import DropDown from '../components/DropDown';
 import about from '../assets/about.png';
-import Loader from '../components/Loader';
+import aboutData from '../data/about.json';
 
 const About = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [aboutData, setAboutData] = useState([]);
 
-  useEffect(() => {
-    const fetchData = () => {
-      fetch('../data/about.json')
-        .then((resp) => resp.json())
-        .then((json) => setAboutData(json.data))
-        .then(console.log(aboutData))
-        .then(()=>{if(aboutData.length!==0){setIsLoading(false)}})
-      .catch ((err) =>(console.error('Erreur de fetch :', err)));
-    };
-
-    fetchData()},[aboutData])
-  
 
   return (
     <>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <>
-          <div className="top-picture">
-            <TopPicture src={about} text={''} />
-          </div>
-          <div className=""></div>
-          <DropDown
-            label={(aboutData[0])}
-            content={(aboutData[0])}
-          />
-          <DropDown
-            label={(aboutData[1])}
-            content={(aboutData[1])}
-          />
-          <DropDown
-            label={(aboutData[2])}
-            content={(aboutData[2])}
-          />
-          <DropDown
-            label={(aboutData[3])}
-            content={(aboutData[3])}
-          />
-        </>
-      )}
+      <div className="top-picture">
+        <TopPicture src={about} text={''} />
+      </div>
+      <div className="drop-group">
+        {aboutData.map((obj, index) => {
+          const lbl = Object.keys(obj)[0];
+          const txt = Object.values(obj)[0];
+          const uniqueKey = `dropdown-${index}`;
+
+          return <DropDown key={uniqueKey} label={lbl} content={txt} />;
+        })}
+      </div>
     </>
   );
 };
